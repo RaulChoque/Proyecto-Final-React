e26683e88d6d2524e2c568186ea33b3b2798a669
raul <choquechungararaulmichel@gmail.com>
2023-08-09 03:05:55 -0300

    Projecto final

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const data = [
+	{
+		id: 1,
+		img: 'https://www.soscomputacion.com.ar/17385-thickbox_default/gabinete-pc-gamer-sentey-x10-rgb-acrilico-gaming-usb-30-sin-fuente.jpg',
+		nameProduct: 'Gabinete Sentey X10 RGB',
+		price: 80,
+		quantity: 1,
+	},
+	{
+		id: 2,
+		img: 'https://diamondsystemar.vtexassets.com/arquivos/ids/158386-800-450?v=638162148622270000&width=800&height=450&aspect=true',
+		nameProduct: 'Gabinete DS AGS 11',
+		price: 50,
+		quantity: 1,
+	},
+	{
+		id: 3,
+		img: 'https://d2r9epyceweg5n.cloudfront.net/stores/911/585/products/kumara-k552-white1-705f99c6725c5bc09216488224268686-1024-1024.jpg',
+		nameProduct: 'Teclado mecanico Kumara k552 Blanco',
+		price: 90,
+		quantity: 1,
+	},
+	{
+		id: 4,
+		img: 'https://pointandclick.com.ar/wp-content/uploads/2021/05/0410257BI.jpg',
+		nameProduct: 'Teclado mecanico Logitech G413 Rojo',
+		price: 50,
+		quantity: 1,
+	},
+	{
+		id: 5,
+		img: 'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+		nameProduct: 'Audifonos',
+		price: 20,
+		quantity: 1,
+	},
+	
+];
